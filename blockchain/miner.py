932319837e7d15585479837bca85920a401e8880
Krunal Patel <Pkrunal616@gmail.com>
2020-04-10 13:06:34 -0400

    stuff is working, but keep getting invalid

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -1,6 +1,5 @@
 import hashlib
 import requests
-
 import sys
 
 from uuid import uuid4
@@ -25,6 +24,9 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    last_hash = hashlib.sha256(f"{last_proof}".encode()).hexdigest()
+    while valid_proof(last_hash, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,10 +42,13 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f"{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    return guess_hash[:6] == last_hash[-6:]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # What node are we interacting with?
     if len(sys.argv) > 1:
         node = sys.argv[1]
@@ -58,7 +63,7 @@ if __name__ == '__main__':
     print("ID is", id)
     f.close()
 
-    if id == 'NONAME\n':
+    if id == "NONAME\n":
         print("ERROR: You must change your name in `my_id.txt`!")
         exit()
     # Run forever until interrupted
@@ -66,15 +71,14 @@ if __name__ == '__main__':
         # Get the last proof from the server
         r = requests.get(url=node + "/last_proof")
         data = r.json()
-        new_proof = proof_of_work(data.get('proof'))
+        new_proof = proof_of_work(data.get("proof"))
 
-        post_data = {"proof": new_proof,
-                     "id": id}
+        post_data = {"proof": new_proof, "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
-        if data.get('message') == 'New Block Forged':
+        if data.get("message") == "New Block Forged":
             coins_mined += 1
             print("Total coins mined: " + str(coins_mined))
         else:
-            print(data.get('message'))
+            print(data.get("message"))
